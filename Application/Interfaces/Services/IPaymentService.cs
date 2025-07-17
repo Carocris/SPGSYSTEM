c9566ed8c9963dce085b696533db8def2bbcfa43
Carocris <carolinortiz0416@gmail.com>
2025-07-17 14:22:28 -0400

    feature(services): Servicios e interfaces

---
+++ Application/Interfaces/Services/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface IPaymentService : IGenericService<Payment>
+    {
+        Task<Payment> GetBySaleIdAsync(int saleId);
+    }
+}
---
+++ Application/Services/CustomerService.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+using Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class CustomerService : GenericService<Customer>, ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+            : base(customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<Customer> GetWithSalesAsync(int id)
+        {
+            return await _customerRepository.GetWithSalesAsync(id);
+        }
+    }
+}
